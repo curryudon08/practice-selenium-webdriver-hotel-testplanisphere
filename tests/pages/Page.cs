90da9910a75afbfa74d8326082849ddc5c028e33
udonk <6565941+curryudon08@users.noreply.github.com>
2021-09-26 04:27:40 +0900

    Create

---
+++ tests/pages/Page.cs
@@ -0,0 +1,20 @@
+using System;
+using OpenQA.Selenium;
+
+namespace tests.pages
+{
+    abstract public class Page
+    {
+        protected WebDriver driver;
+
+        public Page(WebDriver driver)
+        {
+            this.driver = driver;
+            if(!this.AtPage()){
+                throw new Exception(string.Join("指定した画面に遷移できません:{0}",this.ToString()));
+            }
+        }
+
+        abstract protected bool AtPage();
+    }
+}

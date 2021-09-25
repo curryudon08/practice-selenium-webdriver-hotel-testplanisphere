e178d32cd569760aaa321755a5cb1014d7e78beb
udonk <6565941+curryudon08@users.noreply.github.com>
2021-09-26 04:27:20 +0900

    Create Utility.cs

---
+++ tests/utils/Utility.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+
+namespace tests.utils
+{
+    public class Utility
+    {
+        private static readonly string WebDriverPath = "D:\\tool\\edgedriver_win64";
+
+        private Utility()
+        {
+        }
+
+        public static WebDriver CreateWebDriver()
+        {
+            //Chromium版Edgeを使用する
+            var options = new EdgeOptions();
+            options.UseChromium = true;
+
+            var driver = new EdgeDriver(WebDriverPath, options);
+            return driver;
+        }
+    }
+}
